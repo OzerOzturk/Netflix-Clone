b1a4667e926ee8110a0eab6343de03c53bd78cc8
OzerOzturk <ozerozturk1636@gmail.com>
2021-12-10 02:09:53 +0300

    header profile with dropdown and chevron added to project.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link as ReactRouterLink} from 'react-router-dom';
 
-import {Background, ButtonLink, Container, Logo} from './styles/header';
+import {Background, ButtonLink, Container, Link, Logo, Group, Feature, FeatureCallOut, Picture, Profile, PlayButton, Dropdown, Text, Search, SearchIcon, SearchInput} from './styles/header';
 
 
 
@@ -9,8 +9,66 @@ export default function Header ({bg = true, children, ...restProps}) {
     return bg ? <Background {...restProps}>{children} </Background> : children;
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps} >{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps} >{children}</Profile>;
+};
+
+Header.Picture = function HeaderPicture({ children, ...restProps }) {
+  return <Picture {...restProps} >{children}</Picture>;
+};
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
+  const [searchActive, setSearchActive] = useState(false);
+
+  return (
+    <Search {...restProps}>
+      <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} data-testid="search-click">
+        <img src="/images/icons/search.png" alt="Search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+        data-testid="search-input"
+      />
+    </Search>
+  );
+};
+
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps} >{children}</Dropdown>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps} >{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps} >{children}</Link>;
+};
+
+
+Header.PlayButton = function HeaderPlayButton({children,...restProps}) {
+    return <PlayButton {...restProps}>{children}</PlayButton>;
+}
+
+
 Header.Frame = function HeaderFrame({children,...restProps}) {
     return <Container {...restProps}>{children}</Container>;
+}
+
+Header.Group = function HeaderGroup({children,...restProps}) {
+    return <Group {...restProps}>{children}</Group>;
 }
 
 Header.ButtonLink = function HeaderButtonLink({children,...restProps}) {
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -15,7 +15,12 @@ export function SelectProfileContainer({ user, setProfile }) {
       <Profiles>
         <Profiles.Title>Who's watching?</Profiles.Title>
         <Profiles.List>
-          <Profiles.User>
+          <Profiles.User  onClick= {() => {
+            setProfile({
+              displayName:user.displayName,
+              photoURL:user.photoURL,
+            })
+          } }>
             <Profiles.Picture src={user.photoURL} />
             <Profiles.Name>{user.displayName}</Profiles.Name>
           </Profiles.User>
